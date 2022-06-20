7b09efd4b3de278c1f4a4189e2462307525020bf
Igor Popov <popov_i_v@mail.ru>
2022-06-20 16:19:12 +0300

    Make HW1

---
+++ T-1-2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ДЗ/1-2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а
+какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"a = {num1}; b = {num2} -> max = {num1}");
+}
+if (num2 > num1)
+{
+    Console.WriteLine($"a = {num1}; b = {num2} -> max = {num2}");
+}
+else
+{
+    Console.WriteLine("Эти числа являются равными.");
+}
---
+++ T-1-8/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ДЗ/1-8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Найдены следующие чётные числа: ");
+if (number == 0)
+{
+    Console.Write(number);
+}
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        if (i != number && i != number - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+Console.WriteLine();
